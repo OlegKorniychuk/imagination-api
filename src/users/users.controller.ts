c44446bdbfc6f46adbca851add9af5bf796d5eae
OlegKorniychuk <kornoleg576@gmail.com>
2025-09-17 12:37:39 +0300

    feat[Users]: update to work with refactored S3 and Images services; add error handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,50 +7,82 @@ import {
   Param,
   Delete,
   Query,
+  NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ImagesService } from 'src/images/images.service';
 import { ImageSearchOptionsDto } from 'src/images/dto/image-search-options.dto';
+import { User } from './entities/user.entity';
+import { S3Image, Image } from 'src/images/entities/image.entity';
+import { S3Service } from 'src/s3/s3.service';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
     private imagesService: ImagesService,
+    private s3Service: S3Service,
   ) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(): Promise<User[]> {
+    return await this.usersService.findAll();
   }
 
   @Get(':id/images')
-  findUserImages(
+  async findUserImages(
     @Query() query: ImageSearchOptionsDto,
     @Param('id') id: string,
-  ) {
-    return this.imagesService.findMany(query, id);
+  ): Promise<S3Image[]> {
+    const images: Image[] = await this.imagesService.findMany(query, id);
+    const imagesWithUrls: S3Image[] = await Promise.all(
+      images.map(async (image) => {
+        const url: string = await this.s3Service.getImageUrl(
+          image.uniqueName,
+          3600,
+        );
+
+        return { ...image, url };
+      }),
+    );
+
+    return imagesWithUrls;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  async findOne(@Param('id') id: string): Promise<User> {
+    const user = await this.usersService.findOne(id);
+
+    if (!user) throw new NotFoundException();
+
+    return user;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    const updatedUser = await this.usersService.update(id, updateUserDto);
+
+    if (!updatedUser) throw new NotFoundException();
+
+    return updatedUser;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+  async remove(@Param('id') id: string): Promise<User> {
+    const deletedUser = await this.usersService.remove(id);
+
+    if (!deletedUser) throw new NotFoundException();
+
+    return deletedUser;
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DrizzleModule } from 'src/drizzle/drizzle.module';
 import { ImagesModule } from 'src/images/images.module';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [DrizzleModule, ImagesModule],
+  imports: [DrizzleModule, ImagesModule, S3Module],
   exports: [UsersService],
 })
 export class UsersModule {}
