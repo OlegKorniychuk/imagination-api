12d1b848626d162bcf65ee1a5ec4b956e507a891
OlegKorniychuk <kornoleg576@gmail.com>
2025-09-21 16:41:16 +0300

    env: update drizzle schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
 
 export default defineConfig({
-  schema: './src/drizzle/schema/*.schema.ts',
+  schema: './src/modules/drizzle/schema/*.schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.db__DATABASE_URL!,
