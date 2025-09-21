0751b641b37b3e4e1f079759b625dfaf2ad313d4
OlegKorniychuk <kornoleg576@gmail.com>
2025-09-21 20:47:37 +0300

    feat: remove app service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,15 @@
 import { Controller, Get, Request } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Public } from 'src/modules/auth/public.decorator';
 
 @ApiTags('Authentication & healthcheck')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Public()
   @Get('/hc')
   @ApiOperation({ summary: 'Health Check' })
   @ApiResponse({ status: 200, description: 'Service is up and running.' })
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return { message: 'Server up and running' };
   }
 }
